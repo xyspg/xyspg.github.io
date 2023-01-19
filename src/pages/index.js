a113b158fccdb83282030067ae648c2e32182c57
Kang Jiaming <kangjm2005@hotmail.com>
2023-01-19 23:51:40 +0800

    adding meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="我家还蛮大的">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
