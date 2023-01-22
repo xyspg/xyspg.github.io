02fc20143887a44cfa1781b4803658c05fb9b697
Kang Jiaming <kangjm2005@hotmail.com>
2023-01-22 16:35:42 +0800

    remove 'edit this line'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,8 +37,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/xyspg/xyspg/blob/main',
+          // editUrl:
+          //   'https://github.com/xyspg/xyspg/blob/main',
         },
         blog: {
           showReadingTime: true,
