f332a674fd6ab547bcd435d908d3e14e7ac9e2f8
Kang Jiaming <kangjm2005@hotmail.com>
2023-02-21 22:06:12 +0800

    OPTIMIZE a page

--- src/pages/tools/sb.js
+++ src/pages/tools/sb.js
@@ -2,25 +2,60 @@ import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import styles from '../index.module.css';
 import React, { useState, createContext, useEffect } from "react";
-const style = {
-    background: "-webkit-linear-gradient(315deg,#42d392 25%,#647eff)",
-    backgroundClip: "text",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
-    fontFamily: "PingFang SC, Calibri, sans-serif",
-    fontSize: "260px",
-    fontWeight: 900,
-    textAlign: "center",
-    margin:"4rem auto",
-};
+function FullscreenH1() {
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    function handleFullscreenClick() {
+        setIsFullscreen(true);
+        document.documentElement.requestFullscreen();
+    }
 
+    const style = {
+        background: "-webkit-linear-gradient(315deg,#42d392 25%,#647eff)",
+        backgroundClip: "text",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+        fontFamily: "PingFang SC, Calibri, sans-serif",
+        fontSize: "260px",
+        fontWeight: 900,
+        textAlign: "center",
+        margin: "4rem auto",
+    };
+    const style1 = {
+        background: "-webkit-linear-gradient(315deg,#42d392 25%,#647eff)",
+        backgroundClip: "text",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+        fontFamily: "PingFang SC, Calibri, sans-serif",
+        fontSize: "600px",
+        fontWeight: 900,
+        textAlign: "center",
+        margin: "4rem auto",
+    };
 
-export default function Home() {
+    if (isFullscreen) {
+        return (
+            <h1
+                style={style1}
+                onClick={() => {
+                    setIsFullscreen(false);
+                    document.exitFullscreen();
+                }}
+            >
+                SB
+            </h1>
+        );
+    }
     return (
         <Layout>
             <main>
-                <h1 style={style}>SB</h1>
+                <div onClick={handleFullscreenClick}>
+                    <h1 style={style}>SB</h1>
+                </div>
             </main>
         </Layout>
+
     );
 }
+
+export default FullscreenH1;
+
