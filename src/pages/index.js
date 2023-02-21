4646dff86f331201502b5720f12bc52396da2cac
Kang Jiaming <kangjm2005@hotmail.com>
2023-02-21 17:08:40 +0800

    OPTIMIZE article; delete unused declarations

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import * as THREE from 'three'
-import FOG from 'vanta/dist/vanta.fog.min'
 import styles from './index.module.css';
-import { Auth0Provider } from "@auth0/auth0-react";
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
